000b3d5dca43bf762583f811d06858409e66441f
Амина Аруева <arueva.ak@phystech.edu>
2024-10-16 04:29:43 +0300

    +загрузка данных на сайт; +навигация по сайту; +карта Яндекс; +вывод pd.DataFrame на главную страницу;

--- ML/main/views.py
+++ ML/main/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+import pandas as pd
+from .models import MyFiles
 
 def index(request):
-    return render(request,'main/index.html')
+    data = {
+        "calories": [420, 380, 390],
+        "duration": [50, 40, 45],
+        "name": ['Amina', 'Alex', 'Anton'],
+        "skills": ['web', 'ml', 'python']
+
+    }
+    df_gb = pd.DataFrame(data)
+
+    context = {'df': df_gb}
+
+    return render(request, 'main/index.html', context)
+
+def download(request):
+    if request.POST:
+        print(request.FILES.get('file'))
+    MyFiles.objects.create(
+        file = request.FILES.get('file')
+    )
+    return render(request, 'main/download.html')
+

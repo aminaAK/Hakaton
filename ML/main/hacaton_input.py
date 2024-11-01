3955613c694ee85cfac52cdf37c776186bb8d334
ZhiganovD <91308641+ZhiganovD@users.noreply.github.com>
2024-11-01 17:53:05 +0300

    Update hacaton_input.py

--- ML/main/hacaton_input.py
+++ ML/main/hacaton_input.py
@@ -2,25 +2,24 @@ import pandas as pd
 import datetime
 import os
 from sklearn.preprocessing import LabelEncoder
-from .hacaton_preproc import class_cluster, time_borders, dist_cluster, sell_cluster, optim_clust
-from .hacaton_preproc import preproc_delivery_time, construct_column_from_lookup
+from hacaton_preproc import class_cluster, time_borders, dist_cluster, sell_cluster, optim_clust
+from hacaton_preproc import preproc_delivery_time, construct_column_from_lookup
 
 def lots_distr(input_path, metr=False):
-    try:
-        data_input = pd.read_excel(input_path)
-    except:
-        return pd.DataFrame([])
     
-    
+    data_input = pd.read_excel(input_path)
     
     #change these paths to your local ones when adding!
-    hist_path = 'main/data/processed_Исторические_совершенные_закупки_товаров.csv'
-    df_mtr = pd.read_excel('main/data/Кабель справочник МТР.xlsx')
-    df_deliv = pd.read_excel('main/data/КТ-516 Разделительная ведомость на поставку МТР с учетом нормативных сроков поставки.xlsx', header=23)
-    df_cargo = pd.read_excel('main/data/Справочник грузополучателей.xlsx')
+    hist_path = 'data/processed_Исторические_совершенные_закупки_товаров.csv'
+    df_mtr = pd.read_excel('data/Кабель справочник МТР.xlsx')
+    df_deliv = pd.read_excel('data/КТ-516 Разделительная ведомость на поставку МТР с учетом нормативных сроков поставки.xlsx', header=23)
+    df_cargo = pd.read_excel('data/Справочник грузополучателей.xlsx')
         
     data = preproc_delivery_time(data_input, df_mtr, df_deliv, df_cargo)
     data_hist = pd.read_csv(hist_path)
+    errors = data[data['Ошибка'] != 0]
+    data_error_index = errors.index
+    data = data.drop(index=data_error_index).copy()
     
     label_encoder = LabelEncoder()
     data_preprocessed = pd.DataFrame()
@@ -43,8 +42,8 @@ def lots_distr(input_path, metr=False):
         datasets_preprocessed[Class] = time_borders(datasets_preprocessed[Class])
     
     #make clusters for each distance group (list inside previously mentioned list) 
-    cords_path = 'main/data/cwc.csv'
-    clients_path = 'main/data/clients.csv'
+    cords_path = 'data/cwc.csv'
+    clients_path = 'data/clients.csv'
     error_lines = []
     ds_dist = datasets.copy()
     ds_pr_dist = datasets_preprocessed.copy()
@@ -72,7 +71,42 @@ def lots_distr(input_path, metr=False):
                 
     
     #create lots
-    lots = []
+    # lots = []
+
+    # for Class in ds_dist.keys():
+    #     for i in range(len(ds_dist[Class])):
+    #         data_entries = ds_dist[Class][i]
+    #         prepr_entries = ds_pr_dist[Class][i]
+            
+    #         if isinstance(data_entries, list):
+    #             for j in range(len(data_entries)):
+    #                 data_orig = data_entries[j]
+    #                 data_prepr = prepr_entries[j]
+    #                 optimised_data = optim_clust(data_orig, data_prepr, metr)
+                    
+    #                 if optimised_data is not None:
+    #                     for num in optimised_data['cluster'].unique():
+    #                         lot = optimised_data[optimised_data['cluster'] == num]
+    #                         lots.append(lot)
+    #                 else:
+    #                     print(f"Clustering failed for Class {Class}, i={i}, j={j}")
+    #         else:
+    #             data_orig = data_entries
+    #             data_prepr = prepr_entries
+    #             optimised_data = optim_clust(data_orig, data_prepr, metr)
+                
+    #             if optimised_data is not None:
+    #                 for num in optimised_data['cluster'].unique():
+    #                     lot = optimised_data[optimised_data['cluster'] == num]
+    #                     lots.append(lot)
+    #             else:
+    #                 print(f"Clustering failed for Class {Class}, i={i}")
+                    
+    # #delete redundant columns
+    # for i in range(len(lots)):
+    #     lots[i] = lots[i].drop(columns=['dist', 'cluster'])
+    
+    lots_cut = []
 
     for Class in ds_dist.keys():
         for i in range(len(ds_dist[Class])):
@@ -82,30 +116,17 @@ def lots_distr(input_path, metr=False):
             if isinstance(data_entries, list):
                 for j in range(len(data_entries)):
                     data_orig = data_entries[j]
-                    data_prepr = prepr_entries[j]
-                    optimised_data = optim_clust(data_orig, data_prepr, metr)
-                    
-                    if optimised_data is not None:
-                        for num in optimised_data['cluster'].unique():
-                            lot = optimised_data[optimised_data['cluster'] == num]
-                            lots.append(lot)
-                    else:
-                        print(f"Clustering failed for Class {Class}, i={i}, j={j}")
+                    lots_cut.append(data_orig)
             else:
                 data_orig = data_entries
-                data_prepr = prepr_entries
-                optimised_data = optim_clust(data_orig, data_prepr, metr)
-                
-                if optimised_data is not None:
-                    for num in optimised_data['cluster'].unique():
-                        lot = optimised_data[optimised_data['cluster'] == num]
-                        lots.append(lot)
-                else:
-                    print(f"Clustering failed for Class {Class}, i={i}")
-                    
-    #delete redundant columns
-    for i in range(len(lots)):
-        lots[i] = lots[i].drop(columns=['dist', 'cluster'])
+                lots_cut.append(data_orig)
+    
+    for i in range(len(lots_cut)):
+        lots_cut[i] = lots_cut[i].drop(columns='dist')
 
     
-    return lots
+    return lots_cut
+
+input_path = 'Test_input_file.xlsx'    
+data = lots_distr(input_path, False)
+print(data[20], len(data))
